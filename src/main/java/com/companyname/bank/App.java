1787e61de648c4c93ff0a3968df82d6de6be05bb
dev1 <dev1@email.com>
2023-11-16 12:03:16 +0530

    modify

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github hook trigger..change ");
+        System.out.println( "github hook trigger..new change ");
     }
 }
